0a8d13f5fd8c6d940bfadac9837c5aeb07d49e88
bcjack12 <bcjack@hotmail.com>
2018-10-25 20:30:29 +0000

    morning

--- keras-perceptron/perceptron-single.py
+++ keras-perceptron/perceptron-single.py
@@ -17,17 +17,17 @@ is_five_train = y_train == 5
 is_five_test = y_test == 5
 labels = ["Not Five", "Is Five"]
 
-img_width = X_train.shape[1]
+img_width = X_train.shape[1]# 28x28
 img_height = X_train.shape[2]
 
 # create model
 model=Sequential()
-model.add(Flatten(input_shape=(img_width,img_height)))
-model.add(Dense(1))
+model.add(Flatten(input_shape=(img_width,img_height)))#1x784
+model.add(Dense(1, activation='sigmoid')) #makes a single perceptron; one output
 model.compile(loss='mse', optimizer='adam',
                 metrics=['accuracy'])
 
 # Fit the model
-model.fit(X_train, is_five_train, epochs=3, validation_data=(X_test, is_five_test),
+model.fit(X_train, is_five_train, epochs=5, validation_data=(X_test, is_five_test),
                     callbacks=[WandbCallback(data_type="image", labels=labels, save_model=False)])
 model.save('perceptron.h5')
